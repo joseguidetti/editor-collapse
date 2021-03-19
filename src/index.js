6493b35d86cfad1a26c70d1af169e82ac4a2cfab
José Guidetti <jose.guidetti@portalsolar.com.br>
2021-03-19 17:23:00 -0300

    added translate pt-br on tooltip

--- src/index.js
+++ src/index.js
@@ -181,7 +181,7 @@ export default class Collapse {
   static get toolbox() {
     return {
       icon: `<svg width="19" t="1575117780012" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="4865" width="200" height="200"><path d="M512 576l192 192H576v192H448v-192H320l192-192z m192-384H576V0H448v192H320l192 192 192-192z m256 128c0-35.2-28.8-64-64-64h-160l-64 64h192l-128 128h-448l-128-128h192l-64-64H128c-35.2 0-64 28.8-64 64l160 160L64 640c0 35.2 28.8 64 64 64h160l64-64h-192l128-128h448l128 128h-192l64 64H896c35.2 0 64-28.8 64-64l-160-160L960 320z" p-id="4866"></path></svg>`,
-      title: "折叠块",
+      title: "Collapse",
     };
   }
 }
